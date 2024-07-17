5e0b75172cae780fb62d305a7bcad6ad5b352d84
Kirill Fatykov <kirill.fatykov@saritasa.com>
2024-07-17 14:08:14 +0500

    update index.ts JC-626

--- apps/vanilla/src/pages/blackJack/index.ts
+++ apps/vanilla/src/pages/blackJack/index.ts
@@ -3,10 +3,10 @@ import { TurnGenerator } from './models/turnGenerator';
 import { Player } from './models/player';
 import { AllResultsRollDice } from './models/allResultsRollDice';
 
-const btnRollTheDice: HTMLButtonElement | null = document.querySelector('.game__roll-the-dice') as HTMLButtonElement;
-const containerPlayer1: HTMLElement | null = document.getElementById('Player1') as HTMLElement;
-const containerPlayer2: HTMLElement | null = document.getElementById('Player2') as HTMLElement;
-const containerAll: HTMLElement | null = document.getElementById('allResults') as HTMLElement;
+const btnRollTheDice = document.querySelector<HTMLButtonElement>('.game__roll-the-dice');
+const containerPlayer1 = document.getElementById('Player1');
+const containerPlayer2 = document.getElementById('Player2');
+const containerAll = document.getElementById('allResults');
 
 const turnGenerator = new TurnGenerator();
 const diceGenerator = new DiceGenerator();
@@ -24,22 +24,26 @@ player2.subscribe(allResults);
 
 btnRollTheDice?.addEventListener('click', () => {
 
-	/**
-		* Function display in web-site dice resulte of PLayers.
-		* @param player - Class Player.
-		* @param containerPlayer - HTMLElement.
-		*/
-	function displayDiceResultAndWinner(player: Player, containerPlayer: HTMLElement): void {
-		if (player.getWinStatus()) {
-			containerPlayer?.closest('.game__item')?.setAttribute('style', 'background-color:red;');
-			btnRollTheDice?.setAttribute('disabled', 'true');
-		}
-		containerPlayer.innerText = player.diceResults.join(' ');
+	turnGenerator.next();
+	if (containerAll != null) {
+		containerAll.innerText = allResults.allDice.join(' ');
 	}
 
-	turnGenerator.next();
-	containerAll.innerText = allResults.allDice.join(' ');
-
-	displayDiceResultAndWinner(player1, containerPlayer1);
-	displayDiceResultAndWinner(player2, containerPlayer2);
+	if (containerPlayer1 != null && containerPlayer2 != null) {
+		displayDiceResultAndWinner(player1, containerPlayer1);
+		displayDiceResultAndWinner(player2, containerPlayer2);
+	}
 });
+
+/**
+	* Function display in web-site dice resulte of PLayers.
+	* @param player - Class Player.
+	* @param containerPlayer - HTMLElement.
+	*/
+function displayDiceResultAndWinner(player: Player, containerPlayer: HTMLElement): void {
+	if (player.getWinStatus()) {
+		containerPlayer.closest('.game__item')?.setAttribute('style', 'background-color:red;');
+		btnRollTheDice?.setAttribute('disabled', 'true');
+	}
+	containerPlayer.innerText = player.diceResults.join(' ');
+}

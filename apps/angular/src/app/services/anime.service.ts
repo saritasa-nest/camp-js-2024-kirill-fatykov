11df0dacbf1b64f8f36fb853249edeb5e12a33d3
Kirill Fatykov <kirill.fatykov@saritasa.com>
2024-07-18 14:38:09 +0500

    create project JC-641

---
+++ apps/angular/src/app/services/anime.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+// @Injectable({
+// 	providedIn: 'root',
+// })
+// export class ProfileService {
+// 	http: HttpClient = inject(HttpClient);
+
+// 	getAnime(){
+// 		this.http.get()
+// 	}
+// }

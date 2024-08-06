1ed2fba70dcc6c13815307b16c2b9e2762c5295d
Kirill Fatykov <kirill.fatykov@saritasa.com>
2024-08-06 16:51:40 +0500

    update ApiCofig JC-641

--- apps/angular/src/core/services/api-config.service.ts
+++ apps/angular/src/core/services/api-config.service.ts
@@ -4,7 +4,8 @@ import { environment } from '@js-camp/angular/environments/environment';
 /** ApiKey. */
 @Injectable({
 	providedIn: 'root',
-}) export class ApiConfig {
+})
+export class ApiConfig {
 
 	/** Api-url. */
 	public readonly apiUrl = environment.apiUrl;
